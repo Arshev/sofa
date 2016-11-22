9f340c89e1eaf85f893fae8f477aca7c363cd3de
Artem Shevchenko <ashev08@gmail.com>
2016-11-22 16:33:13 +0200

    Bug fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
   validates :title, length: { minimum: 5, maximum: 255 }
