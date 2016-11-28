d7d2b6bc60ecdd63add808afa84edcd97995a7f2
Artem Shevchenko <ashev08@gmail.com>
2016-11-28 20:04:48 +0200

    Add model User and initial tests

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ end
 
 group :test do
   gem 'shoulda-matchers', '~> 3.1'
+  gem 'capybara'
 end
 
 group :development do
---
+++ spec/acceptance/sign_in_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'User sign in', %q{
+  In order to be able to sign in
+  As an User
+  I want to be able to sign in
+} do
+  
+  scenario 'Registred user try to sign in' do
+    User.create!(email: 'test@test.ru', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'test@test.ru'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'Non-registred user try to signed in' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'wrong@test.ru'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid email or password.'
+    expect(current_path).to eq new_user_session_path
+  end
+
+end
