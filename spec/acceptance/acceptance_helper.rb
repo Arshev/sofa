588071147e9c42564e0624953b6e4957802bf332
Artem Shevchenko <ashev08@gmail.com>
2016-12-08 17:19:14 +0200

    Add ajax errors in answers

---
+++ spec/acceptance/acceptance_helper.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.configure do |config|
+
+  Capybara.javascript_driver = :webkit
+  
+  # Database Cleaner
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.use_transactional_fixtures = false
+
+  config.include AcceptanceHelper, type: :feature
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,12 +32,11 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
-  config.include AcceptanceHelper, type: :feature
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -70,25 +69,4 @@ RSpec.configure do |config|
     with.library :rails
   end
 end
-
-# Database Cleaner
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 end
