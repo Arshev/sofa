2a6fb332f1fb9b166dbc3a8395892e6b44cc6ba5
Artem Shevchenko <ashev08@gmail.com>
2017-01-16 17:58:02 +0200

    Question accepts nested attributes for attachments

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -7,4 +7,5 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :body }
   it { should validate_length_of(:title).is_at_least(5) }
   it { should validate_length_of(:title).is_at_most(255) }
+  it { should accept_nested_attributes_for :attachments }
 end
