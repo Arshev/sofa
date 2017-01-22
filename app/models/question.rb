08e33354834234ec41a121e27b50b873f8a31006
Artem Shevchenko <ashev08@gmail.com>
2017-01-22 12:09:37 +0200

    Polymorphic attachments. Answers associated with attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
 
   has_many :answers, dependent: :destroy
-  has_many :attachments, dependent: :destroy
+  has_many :attachments, as: :attachmentable, dependent: :destroy
 
   validates :title, :body, presence: true
   validates :title, length: { minimum: 5, maximum: 255 }
