0052b8f594835e4c5e2dd271b938f5e92b3fc700
Artem Shevchenko <ashev08@gmail.com>
2017-01-26 20:10:16 +0200

    Fix tests

--- spec/acceptance/add_files_to_answer_spec.rb
+++ spec/acceptance/add_files_to_answer_spec.rb
@@ -18,14 +18,14 @@ feature 'Add files to answer', %q{
     fill_in 'Body', with: 'Test answer content'
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
     click_on '+ Add file'
-    within '' do
+    within all('.nested-fields').first do
       attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
     end
     click_on 'Add Answer'
 
     within '.answers' do
-      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+      expect(page).to have_content 'spec_helper.rb'
+      expect(page).to have_content 'rails_helper.rb'    
     end
   end
 
