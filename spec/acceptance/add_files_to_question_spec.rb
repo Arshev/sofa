614dce9d40b13c815af597102fa5015082c381fd
Artem Shevchenko <ashev08@gmail.com>
2017-01-26 20:10:16 +0200

    Fix tests

--- spec/acceptance/add_files_to_question_spec.rb
+++ spec/acceptance/add_files_to_question_spec.rb
@@ -16,17 +16,16 @@ feature 'Add files to question', %q{
   scenario 'User add file when ask question', js: true do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'Test content'
-
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    click_on '+ Add file'
-    within '' do
-     attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    click_on 'Add file'
+    within all('.nested-fields').first do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
     end
 
     click_on 'Create'
 
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+    expect(page).to have_content 'spec_helper.rb'
+    expect(page).to have_content 'rails_helper.rb'
   end
 
 end
