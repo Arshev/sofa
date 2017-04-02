8b929413b2bd521a447dd76c6df6e80a671d46bf
Artem Shevchenko <ashev08@gmail.com>
2017-04-02 12:36:25 +0200

    Add shared examples for Voting controllers

--- spec/controllers/vote_answer_controller_spec.rb
+++ spec/controllers/vote_answer_controller_spec.rb
@@ -4,70 +4,7 @@ RSpec.describe AnswersController, type: :controller do
 
   let!(:question) { create(:question) }
   let!(:answer) { create(:answer, question: question) }
+  let(:controller_type) { answer }
 
-  describe 'PATCH #vote_up' do    
-
-    context 'when user votes for someones answer' do
-
-      sign_in_user
-      before { patch :vote_up, params: {id: answer} }
-
-      it 'increase rating by 1' do
-        result = {"rating":1, "vote":1}.to_json
-        expect(response.body).to eq(result)
-      end
-
-      it 'responds with json' do
-        expect(response.content_type).to eq('application/json')
-      end
-
-      it 'save vote to db' do
-        resp = JSON.parse(@response.body)
-        expect(resp["vote"]).to eq(Vote.first.value)
-      end
-    end
-    context 'when user votes for own answer' do
-      let(:users_record) { create(:answer, user: @user) }
-      sign_in_user
-      before { patch :vote_up, params: {id: answer} }
-      it 'increase rating by 0' do
-        result = {"rating":0, "vote":0}.to_json
-        expect(response.body).to eq(result)
-      end
-    end
-  end
-
-  describe 'PATCH #vote_down' do    
-
-    context 'when user votes for someones answer' do
-
-      sign_in_user
-      before { patch :vote_down, params: {id: answer} }
-
-      it 'increase rating by 1' do
-        result = {"rating": -1, "vote": -1}.to_json
-        expect(response.body).to eq(result)
-      end
-
-      it 'responds with json' do
-        expect(response.content_type).to eq('application/json')
-      end
-
-      it 'save vote to db' do
-        resp = JSON.parse(@response.body)
-        expect(resp["vote"]).to eq(Vote.first.value)
-      end
-    end
-
-    context 'when user votes for own answer' do
-      let!(:user) {create(:user)}
-      let!(:users_answer) { create(:answer, user: user, question: question) }
-      sign_in_user
-      before { patch :vote_down, params: {id: users_answer} }
-      it 'increase rating by 0' do
-        result = {"rating":0, "vote":0}.to_json
-        expect(response.body).to eq(result)
-      end
-    end
-  end
+  it_behaves_like 'Vote controllers'
 end
--- spec/controllers/vote_questions_controller_spec.rb
+++ spec/controllers/vote_questions_controller_spec.rb
@@ -3,69 +3,7 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
 
   let!(:question) { create(:question) }
+  let(:controller_type) { question }
 
-  describe 'PATCH #vote_up' do    
-
-    context 'when user votes for someones question' do
-
-      sign_in_user
-      before { patch :vote_up, params: {id: question} }
-
-      it 'increase rating by 1' do
-        result = {"rating":1, "vote":1}.to_json
-        expect(response.body).to eq(result)
-      end
-
-      it 'responds with json' do
-        expect(response.content_type).to eq('application/json')
-      end
-
-      it 'save vote to db' do
-        resp = JSON.parse(@response.body)
-        expect(resp["vote"]).to eq(Vote.first.value)
-      end
-    end
-    context 'when user votes for own question' do
-      let(:users_record) { create(:question, user: @user) }
-      sign_in_user
-      before { patch :vote_up, params: {id: question} }
-      it 'increase rating by 0' do
-        result = {"rating":0, "vote":0}.to_json
-        expect(response.body).to eq(result)
-      end
-    end
-  end
-
-  describe 'PATCH #vote_down' do    
-
-    context 'when user votes for someones question' do
-
-      sign_in_user
-      before { patch :vote_down, params: {id: question} }
-
-      it 'increase rating by 1' do
-        result = {"rating": -1, "vote": -1}.to_json
-        expect(response.body).to eq(result)
-      end
-
-      it 'responds with json' do
-        expect(response.content_type).to eq('application/json')
-      end
-
-      it 'save vote to db' do
-        resp = JSON.parse(@response.body)
-        expect(resp["vote"]).to eq(Vote.first.value)
-      end
-    end
-
-    context 'when user votes for own question' do
-      let(:users_record) { create(:question, user: @user) }
-      sign_in_user
-      before { patch :vote_down, params: {id: question} }
-      it 'increase rating by 0' do
-        result = {"rating":0, "vote":0}.to_json
-        expect(response.body).to eq(result)
-      end
-    end
-  end
+  it_behaves_like 'Vote controllers'
 end
