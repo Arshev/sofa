41759deb435000a94a670f52aa1a2a3c1c919855
Artem Shevchenko <ashev08@gmail.com>
2017-04-13 15:25:12 +0200

    Green models tests

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,28 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Search do
+RSpec.describe Search, type: :sphinx do
   describe '.find' do
-   
-    let(:user) { create(:user) }
-    let!(:question) { create(:question, user: user) }
-    let(:search) { create(:search) }
-    let!(:query) { ThinkingSphinx::Query.escape(search.query) }
+    
+    let(:query) { 'Test' }
 
-    it 'global search when scope not defined' do
-      expect(ThinkingSphinx).to receive(:search).with(query)
-      search.find
+    it 'receive data' do
+        ts_query = ThinkingSphinx::Query.escape(query)
+        expect(ThinkingSphinx::Query).to receive(:escape).with(query).and_call_original
+        expect(ThinkingSphinx).to receive(:search).with(ts_query)
+        Search.find(query, "everywhere")
     end
+  end
 
-    # it 'searches for specific models when scope defined' do
-    #   search.scope = 'questions'
-    #   expect(Question).to receive(:search).with(query)
-    #   search.run
-    # end
-
-    # it 'stores search results in @result' do
-    #   allow(ThinkingSphinx).to receive(:search).and_return([question])
-    #   search.run
-    #   expect(search.result).to eq([question])
-    # end
+  describe "Invalid params" do    
+    it 'returns empty arr if context is invalid' do
+      expect(Search.find('', 'else')).to match_array []
+    end
   end
 end
