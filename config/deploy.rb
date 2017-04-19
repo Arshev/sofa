8bab09ae76c8ad6f9b7714e3c63fd43f8aa819e5
Artem Shevchenko <ashev08@gmail.com>
2017-04-19 16:48:03 +0200

    Good deploy

---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+# config valid only for current version of Capistrano
+lock "3.8.0"
+
+set :application, "SOFA"
+set :repo_url, "git@github.com:Arshev/sofa.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+
+# Default value for :format is :airbrussh.
+set :format, :pretty
+
+set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+append :linked_files, 'config/database.yml', 'config/production.sphinx.conf', '.env'
+
+# Default value for linked_dirs is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads', 'vendor/bundle'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
